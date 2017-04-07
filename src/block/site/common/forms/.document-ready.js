1fdbdce8fe413c27e0548c45d5b66cafd7470f40
azbn <i@azbn.ru>
2017-04-07 13:31:36 +0300

    201704071331

--- src/block/site/common/forms/.document-ready.js
+++ src/block/site/common/forms/.document-ready.js
@@ -1,19 +1,37 @@
 
-
-
 $(document.body).on('submit.azbn', '#project-append form', {}, function(event){
 	event.preventDefault();
 	
 	var form = $(this);
+	
+	var uniq = $(document.body).attr('data-uniq') || '';
+	var uniq_i = $('<input/>', {
+		name : 'uniq',
+		value : uniq,
+		type : 'hidden',
+	});
+	
 	var sform = form.serialize();
 	
-	var target = form.attr('data-target') || '/';
+	var _target = form.attr('data-target') || '/';
 	
-	$.post(target, sform, function(data){
+	$.post(_target, sform, function(data){
+		
+		form
+			.find('input[name="uniq"]')
+				.empty()
+				.remove()
+		;
+		form
+			.trigger('reset')
+		;
 		
 		form.closest('.content-modal').find('.close-btn').trigger('click');
 		
-		$(document.body).on('azbn.content-modal.open', ['#project-appended']);
+		setTimeout(function(){
+			$(document.body).trigger('azbn.content-modal.open', ['#project-appended']);
+		}, 333);
+		
 		
 	});
 	
--- src/block/site/common/modals/.document-ready.js
+++ src/block/site/common/modals/.document-ready.js
@@ -2,11 +2,11 @@
 
 
 $(document.body).on('click.azbn', '.content-modal', {}, function(event){
-	event.preventDefault();
 	
 	var _target = $(event.target);
 	
 	if( _target.hasClass('close-btn') || _target.hasClass('content-modal')) {
+		event.preventDefault();
 		
 		$(document.body).removeClass('modalled');
 		$('.content-modal').removeClass('active');
