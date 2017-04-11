9b704bf61b9a8ad10915ae2979c0ec42766094c0
azbn <i@azbn.ru>
2017-04-11 14:21:33 +0300

    201704111421

--- src/_/concat.window-resize.js
+++ src/_/concat.window-resize.js
@@ -1,4 +1,4 @@
-"use strict";
-"use strict";
-"use strict";$(".b-projects").trigger("azbn.render.b-projects");
-"use strict";!function(){if($(".mcs-content").length){var t=$(".mcs-content");t.mCustomScrollbar("destroy"),t.each(function(t){var o=$(this);$(".mcs-content").mCustomScrollbar({axis:"y",setHeight:o.closest(".modal-content").outerHeight(!0)-90+"px",alwaysShowScrollbar:2,live:!0})})}}();
+
+
+$(".b-projects").trigger("azbn.render.b-projects");
+!function(){if($(".mcs-content").length){var t=$(".mcs-content");t.mCustomScrollbar("destroy"),t.each(function(t){var o=$(this);$(".mcs-content").mCustomScrollbar({axis:"y",setHeight:o.closest(".modal-content").outerHeight(!0)-90+"px",alwaysShowScrollbar:2,live:!0})})}}();
